9f8a5b807d21f0a604bde94a0cc417f6c84e79c9
TewariSagar <sagar9911102314@gmail.com>
2016-06-11 13:25:24 -0400

    Done problem 1.2 using Arrays sort O(nlogn)

--- permutation.java
+++ permutation.java
@@ -7,6 +7,33 @@ import java.util.*;
 class permutation {
 
 	public static void main(String[] args) {
-		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the first string");
+		String firstString = s.next();
+		System.out.println("Enter the string you want to test with");
+		String secondString = s.next();
+		boolean ret = checkPermutation(firstString, secondString);
+		if(ret)
+			System.out.println("YES it is a permutation");
+		else
+			System.out.println("NO it is not a permutation");
+	}
+
+	static boolean checkPermutation(String firstString, String secondString){
+		if(firstString.length() != secondString.length())
+			return false;
+		Character arr[] = new Character[firstString.length()];
+		Character arr2[] = new Character[secondString.length()];
+		for(int i =0; i < firstString.length(); i++)
+		{
+			arr[i] = firstString.charAt(i);
+			arr2[i] = secondString.charAt(i);
+		}
+		Arrays.sort(arr);
+		Arrays.sort(arr2);
+		if(Arrays.equals(arr,arr2))
+		return true;
+		else
+			return false;
 	}
 }
